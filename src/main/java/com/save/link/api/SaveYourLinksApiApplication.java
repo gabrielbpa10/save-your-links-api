da611a99b143d48e0c9a6808aed57a4620e3d1e7
Gabriel Araujo <gabrielaraujoprofissionalti@gmail.com>
2026-09-21 21:06:39 -0300

    Inicial commit

---
+++ src/main/java/com/save/link/api/SaveYourLinksApiApplication.java
@@ -0,0 +1,13 @@
+package com.save.link.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveYourLinksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveYourLinksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/save/link/api/SaveYourLinksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.save.link.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveYourLinksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
